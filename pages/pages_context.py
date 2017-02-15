4416edca73257327901e31da4f2f507252f0734b
Katerina Kampardi <kkampardi@gmail.com>
2017-02-15 12:53:04 +0200

    Fix localization for pages menu #23

--- pages/pages_context.py
+++ pages/pages_context.py
@@ -1,16 +1,19 @@
 
 from portfolio.models import Project
 from products.models import Product, Category
-from pages.models import Slider
+from pages.models import Slider, Page
 
 def show_fp_projects(request):
     fp_projects = Project.objects.all()[:3]
     latest_project = Project.objects.all()[:1]
     slides = Slider.objects.all()
+    pages_menu = Page.objects.all()
+
     return {
         'fp_projects': fp_projects,
         'latest_project': latest_project,
         'slides': slides,
+        'pages_menu' : pages_menu,
         }
 
 def show_fp_products(request):
